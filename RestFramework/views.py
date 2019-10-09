50a3a86113ebe6186b0e051801db52b760c6ebe9
loingjuzy <loingjuzy@qq.com>
2019-10-09 17:35:07 +0800

    second commit

---
+++ RestFramework/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.views import APIView
+from RestFramework import models
+
+
+def md5(user):
+    import hashlib
+    import time
+    # 当前时间，相当于生成一个随机的字符串
+    ctime = str(time.time())
+    m = hashlib.md5(bytes(user, encoding='utf-8'))
+    m.update(bytes(ctime, encoding='utf-8'))
+    return m.hexdigest()
+
+
+class AuthView(APIView):
+    def post(self, request, *args, **kwargs):
+        ret = {'code': 1000, 'msg': None}
+        try:
+            user = request._request.POST.get('username')
+            pwd = request._request.POST.get('password')
+            obj = models.UserInfo.objects.filter(username=user, password=pwd).first()
+            if not obj:
+                ret['code'] = 1001
+                ret['msg'] = '用户名或密码错误'
+                return JsonResponse(ret)
+                # 为用户创建token
+            token = md5(user)
+            # 存在就更新，不存在就创建
+            models.UserToken.objects.update_or_create(user=obj, defaults={'token': token})
+            ret['token'] = token
+        except Exception as e:
+            ret['code'] = 1002
+            ret['msg'] = '请求异常'
+        return JsonResponse(ret)
